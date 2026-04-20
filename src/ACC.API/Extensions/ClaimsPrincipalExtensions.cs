f6ddc269e9a4dfe8b1bad6351668a319ef7d7292
Shermann <unespageti@gmail.com>
2026-04-19 22:09:49 -0600

    feat: agregar extensión para obtener ID de usuario desde claims principal

---
+++ src/ACC.API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,11 @@
+using System.Security.Claims;
+
+namespace ACC.API.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string? GetUserId(this ClaimsPrincipal user)
+    {
+        return user.FindFirstValue(ClaimTypes.NameIdentifier) ?? user.FindFirstValue("sub");
+    }
+}
